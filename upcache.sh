37e18e18e980a81673203f6718f3c32b6ee8c8bd
Andika Triwidada <andika@gmail.com>
2016-08-23 11:34:22 +0700

    update local lxc template cache

---
+++ upcache.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# based on http://www.panticz.de/Update-LXC-container-templates-filesystem
+
+# ensure that this script is run as root
+if [ $(id -u) -ne 0 ]; then
+  sudo $0
+  exit
+fi
+
+# set language to english
+LANG=en_US.UTF-8
+
+# update APT rootfs (Debian and Ubuntu)
+for DIST in $(find /var/cache/lxc/*/* -maxdepth 0 -type d|grep -v download); do
+  date
+  echo "Updating ${DIST} ..."
+  chroot "${DIST}" apt-get update -qq
+  chroot "${DIST}" apt-get dist-upgrade -qq -y
+  chroot "${DIST}" apt-get autoremove -qq -y
+  chroot "${DIST}" apt-get clean
+done
