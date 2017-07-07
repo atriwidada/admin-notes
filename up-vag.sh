1160949ccbbcef8b5484c0b86a5fc7da66192815
Andika Triwidada <andika@gmail.com>
2017-07-07 20:02:56 +0700

    vagrant update also remove older boxes

--- up-vag.sh
+++ up-vag.sh
@@ -1,14 +1,36 @@
-#!/bin/sh
-#for b in `vagrant box list|grep -v ', 0)'|awk '{print $1}'|sort|uniq`
+#!/bin/bash
+# update vagrant boxes
+
 # handle multiple providers
 for b in `vagrant box list |grep -v ', 0)'|awk '{print $1i"_"$2}'|sed 's/(//'|sed 's/,//'|sort|uniq`
 do 
     date
-#    echo "line=" $b
+    #echo "line=" $b
     BOX=`echo $b|cut -f 1 -d '_'`
     PROV=`echo $b|cut -f 2 -d '_'`
     echo $BOX $PROV
-    #vagrant box update --box $b
     vagrant box update --box $BOX --provider $PROV
 done
 
+
+# remove old version of vagrant boxes
+TMPFILE=`tempfile`
+vagrant box list > $TMPFILE 
+first='1'
+while read -r vname vtype vver || [[ -n "$vver" ]]; do
+    vtype=`echo $vtype | sed -e 's/[\(\),]//g'`
+    vver=`echo $vver | sed -e 's/[\(\),]//g'`
+    #echo cur: $vname $vtype $vver prev: $pname $ptype $pver
+    if [ .$first = '.1' ] ; then
+        first=0
+    else
+        if ([ .$pname = .$vname ] && [ .$ptype = .$vtype ]) ; then 
+            echo will try to remove older $pname $ptype version $pver and keep newer version $vver
+            vagrant box remove $pname --provider $ptype --box-version $pver
+        fi
+    fi
+    pname=$vname
+    ptype=$vtype
+    pver=$vver
+done < $TMPFILE
+rm $TMPFILE
