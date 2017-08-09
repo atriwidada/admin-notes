e709cd875a6e27dae218729ac466cfc143dee54c
Andika Triwidada <andika@gmail.com>
2017-08-09 14:11:48 +0700

    updated lxc-apt, handling stopped VMs

--- lxc-apt.sh
+++ lxc-apt.sh
@@ -2,6 +2,7 @@
 # package update for each lxc VMs
 
 echo "lxc VM package update"
+echo "---------------------"
 echo "update packages for each running VMs"
 for m in `lxc-ls -1 --running`
 do
@@ -9,15 +10,22 @@ do
 	echo $m
 	lxc-attach -n $m -- apt update
 	lxc-attach -n $m -- apt upgrade
+	echo "====================="
 done
 
 echo "update packages for each stopped VMs"
-for m in `lxc-ls -1 --stopped`
+for s in `lxc-ls -1 --stopped`
 do
 	date
-	echo $m
-	lxc-execute -n $m -- apt update
-	lxc-execute -n $m -- apt upgrade
+	echo $s
+	echo "starting $s..."
+	lxc-start -d -n $s
+	sleep 5
+	lxc-attach -n $s -- apt update
+	lxc-attach -n $s -- apt upgrade
+	echo "stopping $s..."
+	lxc-stop -n $s
+	echo "====================="
 done
 
 
