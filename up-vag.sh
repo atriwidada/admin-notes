4a518ebee1bba4c21071413dc82245cb3559f19a
Andika Triwidada <andika@gmail.com>
2018-05-13 18:27:49 +0700

    bugfix

--- up-vag.sh
+++ up-vag.sh
@@ -9,13 +9,13 @@ DEBUG=1
 # initial box list
 TMPBOXLISTINIT=$(mktemp)
 vagrant box list | sort > $TMPBOXLISTINIT
-if [ .$DEBUG = '.1' ] then
+if [ .$DEBUG = '.1' ]; then
 	echo "box list at start"
 	cat $TMPBOXLISTINIT
 fi
 
 # handle multiple providers
-if [ .$DEBUG = '.1' ] then
+if [ .$DEBUG = '.1' ]; then
 	echo "trying to update boxes"
 fi
 for b in `cat $TMPBOXLISTINIT|grep -v ', 0)'|awk '{print $1i"_"$2}'|sed 's/(//'|sed 's/,//'|sort|uniq`
@@ -31,25 +31,24 @@ done
 # remove old version of vagrant boxes
 TMPFILE=$(mktemp)
 vagrant box list | sort > $TMPFILE 
-if [ .$DEBUG = '.1' ] then
+if [ .$DEBUG = '.1' ]; then
 	echo "boxes list after update"
 	cat $TMPFILE
 fi
-cat $TMPFILE
 echo "remove old boxes"
 first='1'
 while read -r vname vtype vver || [[ -n "$vver" ]]; do
     vtype=`echo $vtype | sed -e 's/[\(\),]//g'`
     vver=`echo $vver | sed -e 's/[\(\),]//g'`
     # echo cur: $vname $vtype $vver prev: $pname $ptype $pver
-    if [ .$first = '.1' ] ; then
+    if [ .$first = '.1' ]; then
         first=0
     else
-        if ([ .$pname = .$vname ] && [ .$ptype = .$vtype ]) ; then 
+        if ([ .$pname = .$vname ] && [ .$ptype = .$vtype ]); then 
             echo -n "will try to remove older $pname $ptype version $pver and keep newer version $vver: "
             vagrant box remove $pname --provider $ptype --box-version $pver 2>/dev/null
 	    ret=$?
-	    if [ .$ret != .0 ] ; then
+	    if [ .$ret != .0 ]; then
 		echo "removal failed, box is in use"
 	    else
 		echo "success"
@@ -66,7 +65,7 @@ TMPBOXLISTFIN=$(mktemp)
 vagrant box list | sort > $TMPBOXLISTFIN
 
 # list changes
-if [ .$DEBUG = '.1' ] then
+if [ .$DEBUG = '.1' ]; then
 	echo "boxes list before update vs after old removal"
 	cat $TMPFILE
 	sdiff -s $TMPBOXLISTINIT $TMPBOXLISTFIN
