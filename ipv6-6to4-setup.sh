927b3ea1ccf640715b7f34f79be121d3de16c7af
Andika Triwidada <andika@gmail.com>
2018-08-30 13:13:50 +0700

    add host prerequisite

--- ipv6-6to4-setup.sh
+++ ipv6-6to4-setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # setup IPv6 6to4 on ancient distro :D
+# prerequisite: host must have public IPv4 address
 
 ipv4=`ip a | grep inet | grep -v inet6 | awk '{print $2}' | egrep -ve '^(127|10|100\.6[4-9]|100\.[7-9][0-9]|100\.1[0-1][0-9]|100\.12[0-7]|172\.1[6-9]|172\.2[0-9]|172\.3[0-1]|192\.168)\.' | sed -e 's/\/.*$//'`
 ipv6=$(printf "2002:%02x%02x:%02x%02x::1" `echo $ipv4 | tr "." " "`)
